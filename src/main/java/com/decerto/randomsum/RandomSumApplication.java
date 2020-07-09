37b8052d9cf8131bb1d0a941761ccdb95dbca63e
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-07-09 20:07:33 +0200

    Initial commit

---
+++ src/main/java/com/decerto/randomsum/RandomSumApplication.java
@@ -0,0 +1,13 @@
+package com.decerto.randomsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomSumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomSumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/decerto/randomsum/RandomSumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decerto.randomsum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomSumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
